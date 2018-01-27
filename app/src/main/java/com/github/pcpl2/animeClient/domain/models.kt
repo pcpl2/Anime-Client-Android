2284e8d9e56eed50f9abb1e15b81e352a0a9d688
pcpl2 <patryk@bimgam.es>
2018-01-27 03:56:35 +0100

    Changed episode entry model.

--- app/src/main/java/com/github/pcpl2/animeClient/domain/models.kt
+++ app/src/main/java/com/github/pcpl2/animeClient/domain/models.kt
@@ -6,6 +6,6 @@ package com.github.pcpl2.animeClient.domain
 
 data class AnimeEntry(val id: String, val url: String, val title: String, val img: String, val desc: String)
 
-data class EpisodeEntry(val id: String, val url: String, val title: String, var players: Array<PlayerEntry>)
+data class EpisodeEntry(val id: String, val url: String, val title: String)
 
 data class PlayerEntry(val id: String, val url: String, val lang: String, val name: String, val desc: String, val referer: String)
