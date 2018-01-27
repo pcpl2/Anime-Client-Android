bba8a436af85c99d6a65bc4d7c0392c4beaf3804
pcpl2 <patryk@bimgam.es>
2018-01-27 04:00:18 +0100

    Small fix for commit 4f001492d51756fb9e787470d0de58cf972ee61a

--- app/src/main/java/com/github/pcpl2/animeClient/callbacks/AnimeFragmentsCallbacks.kt
+++ app/src/main/java/com/github/pcpl2/animeClient/callbacks/AnimeFragmentsCallbacks.kt
@@ -1,9 +1,10 @@
 package com.github.pcpl2.animeClient.callbacks
 
+import com.github.pcpl2.animeClient.domain.AnimeEntry
 /**
  * Created by patry on 26.01.2018.
  */
 
 interface AnimeListFragmentElementClicked {
-    fun onClicked( animeId: String )
+    fun onClicked(animeEntry: AnimeEntry)
 }
